3cbe46b9f96b44937d504c8d9b39624ef5031996
lochana <lochanamethsilu@gmail.com>
2025-11-09 20:58:42 +0530

    add logo

--- lochanaa/app/layout.js
+++ lochanaa/app/layout.js
@@ -4,14 +4,16 @@ import CustomCursor from "@/components/CustomCursor";
 
 
 export const metadata = {
-  title: "Lochana",
+  title: "Lochana Methsiluni",
   description: "Personal portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      <head>
+        <link rel="icon" href="/image/logo.ico" />
+      </head>
       <body className="bg-gray-900 text-white min-h-screen flex flex-col">
        <CustomCursor />
         <Navbar />
